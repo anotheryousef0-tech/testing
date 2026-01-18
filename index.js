3fa3780f4b7a9502762d1ba383f9620362b0bc84
anotheryousef0-tech <anotheryousef0@gmail.com>
2026-01-18 15:07:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
-const { commands, storage } = vendetta;
+const { metro, patcher, storage, commands } = vendetta;
+const { React } = metro.common;
 
-export default {
+const MessageHeader = metro.find(m => m.default?.displayName === "MessageHeader");
+const { Text } = metro.common.Components;
+
+// Wpee2L Nickname Plugin
+const plugin = {
     onLoad: () => {
         try {
-            const unregister = commands.registerCommand({
+            plugin.unregisterCommand = commands.registerCommand({
                 name: "setnickname",
-                description: "Save a local nick - Wpee2L",
+                description: "Set a local red nickname (Wpee2L System)",
                 options: [
-                    { name: "user", description: "user", type: 6, required: true },
-                    { name: "nick", description: "nick", type: 3, required: true }
+                    { name: "user", description: "The user to nickname", type: 6, required: true },
+                    { name: "nick", description: "The nickname to display", type: 3, required: true }
                 ],
                 execute: (args) => {
                     const userId = args[0].value;
                     const nickname = args[1].value;
                     storage[userId] = nickname;
-                    return { content: "Saved: " + nickname };
+                    return { content: "Wpee2L System: Nickname set successfully!" };
                 }
             });
-            this.unregister = unregister;
-        } catch (e) {
-            console.error(e);
+
+            plugin.unpatch = patcher.after("default", MessageHeader, ([{ message }], res) => {
+                const customAlias = storage[message.author.id];
+                if (customAlias) {
+                    const headerChildren = res.props.children[1].props.children;
+                    headerChildren.push(
+                        React.createElement(Text, {
+                            style: {
+                                color: "#FF0000",
+                                fontWeight: "bold",
+                                marginLeft: 4,
+                                fontSize: 12,
+                            }
+                        }, "[" + customAlias + "]")
+                    );
+                }
+            });
+        } catch (err) {
+            console.error("Wpee2L Plugin Error:", err);
         }
     },
     onUnload: () => {
-        if (this.unregister) this.unregister();
+        plugin.unregisterCommand?.();
+        plugin.unpatch?.();
     }
 };
+
+export default plugin;
