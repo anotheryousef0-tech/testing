517dbaeed94a7c05e1f09e5bc6191e5d96fd2420
anotheryousef0-tech <anotheryousef0@gmail.com>
2026-01-18 15:09:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,50 @@
-const { metro, patcher, storage, commands } = vendetta;
-const { React } = metro.common;
+import { metro, patcher, storage, commands } from "@vendetta";
+import { React } from "@vendetta/metro/common";
 
 const MessageHeader = metro.find(m => m.default?.displayName === "MessageHeader");
 const { Text } = metro.common.Components;
 
-// Wpee2L Nickname Plugin
-const plugin = {
+export default {
     onLoad: () => {
-        try {
-            plugin.unregisterCommand = commands.registerCommand({
-                name: "setnickname",
-                description: "Set a local red nickname (Wpee2L System)",
-                options: [
-                    { name: "user", description: "The user to nickname", type: 6, required: true },
-                    { name: "nick", description: "The nickname to display", type: 3, required: true }
-                ],
-                execute: (args) => {
-                    const userId = args[0].value;
-                    const nickname = args[1].value;
-                    storage[userId] = nickname;
-                    return { content: "Wpee2L System: Nickname set successfully!" };
-                }
-            });
+        // Plugin by Wpee2L
+        const setNickCommand = commands.registerCommand({
+            name: "setnickname",
+            description: "Set local nick for a user (Wpee2L System)",
+            options: [
+                { name: "user", description: "Select user", type: 6, required: true },
+                { name: "nick", description: "Enter nickname", type: 3, required: true }
+            ],
+            execute: (args) => {
+                const userId = args[0].value;
+                const nickname = args[1].value;
+                storage[userId] = nickname;
+                return { content: "Wpee2L System: Nickname saved locally." };
+            }
+        });
 
-            plugin.unpatch = patcher.after("default", MessageHeader, ([{ message }], res) => {
-                const customAlias = storage[message.author.id];
-                if (customAlias) {
-                    const headerChildren = res.props.children[1].props.children;
-                    headerChildren.push(
-                        React.createElement(Text, {
-                            style: {
-                                color: "#FF0000",
-                                fontWeight: "bold",
-                                marginLeft: 4,
-                                fontSize: 12,
-                            }
-                        }, "[" + customAlias + "]")
-                    );
-                }
-            });
-        } catch (err) {
-            console.error("Wpee2L Plugin Error:", err);
-        }
+        const patch = patcher.after("default", MessageHeader, ([{ message }], res) => {
+            const customAlias = storage[message.author.id];
+            if (customAlias) {
+                const headerChildren = res.props.children[1].props.children;
+                headerChildren.push(
+                    React.createElement(Text, {
+                        style: {
+                            color: "#FF0000",
+                            fontWeight: "bold",
+                            marginLeft: 4,
+                            fontSize: 12,
+                        }
+                    }, `[${customAlias}]`)
+                );
+            }
+        });
+
+        // Store for unloading
+        window.wpee2l_command = setNickCommand;
+        window.wpee2l_patch = patch;
     },
     onUnload: () => {
-        plugin.unregisterCommand?.();
-        plugin.unpatch?.();
+        if (window.wpee2l_command) window.wpee2l_command();
+        if (window.wpee2l_patch) window.wpee2l_patch();
     }
 };
-
-export default plugin;
