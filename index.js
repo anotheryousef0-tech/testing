b0715f8657aac0d3139f65211b4f11f41352d23d
anotheryousef0-tech <anotheryousef0@gmail.com>
2026-01-18 15:13:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-const { metro, patcher, storage } = vendetta;
-const { React } = metro.common;
+(function() {
+    const { metro, patcher, storage } = window.vendetta;
+    const { React } = metro.common;
+    const MessageHeader = metro.find(m => m.default?.displayName === "MessageHeader");
+    const { Text } = metro.common.Components;
 
-const MessageHeader = metro.find(m => m.default?.displayName === "MessageHeader");
-const { Text } = metro.common.Components;
-const ActionSheet = metro.find(m => m.default?.displayName === "ActionSheet" || m.ActionSheet);
-
-const plugin = {
-    onLoad: () => {
-        // 1. إضافة اللقب الأحمر في الشات
-        plugin.unpatchHeader = patcher.after("default", MessageHeader, ([{ message }], res) => {
-            const customNick = storage[message.author.id];
-            if (customNick) {
-                const headerChildren = res.props.children[1].props.children;
-                headerChildren.push(
-                    React.createElement(Text, {
-                        style: { color: "#FF0000", fontWeight: "bold", marginLeft: 4, fontSize: 12 }
-                    }, `[${customNick}]`)
-                );
-            }
-        });
-
-        // 2. تجربة تفعيل بدون أوامر (فقط تعديل الشات)
-    },
-    onUnload: () => {
-        plugin.unpatchHeader?.();
-    }
-};
-
-export default plugin;
+    return {
+        onLoad: () => {
+            // Wpee2L System
+            this.unpatch = patcher.after("default", MessageHeader, ([{ message }], res) => {
+                const nick = storage[message.author.id];
+                if (nick) {
+                    const children = res.props.children[1].props.children;
+                    children.push(
+                        React.createElement(Text, {
+                            style: { color: "#FF0000", fontWeight: "bold", marginLeft: 4, fontSize: 12 }
+                        }, "[" + nick + "]")
+                    );
+                }
+            });
+        },
+        onUnload: () => {
+            if (this.unpatch) this.unpatch();
+        }
+    };
+})();
