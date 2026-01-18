ae427d4e3ad6ffb9327dec20bbcd02477557b8e9
anotheryousef0-tech <anotheryousef0@gmail.com>
2026-01-18 14:56:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,55 +4,57 @@ const { React } = metro.common;
 const MessageHeader = metro.find(m => m.default?.displayName === "MessageHeader");
 const { Text } = metro.common.Components;
 
+function onLoad() {
+    // Plugin by Wpee2L
+    this.unregisterCommand = commands.registerCommand({
+        name: "setnickname",
+        description: "Set a local red nickname (Wpee2L System)",
+        options: [
+            {
+                name: "user",
+                description: "The user to nickname",
+                type: 6,
+                required: true,
+            },
+            {
+                name: "nick",
+                description: "The nickname to display",
+                type: 3,
+                required: true,
+            }
+        ],
+        execute: (args, ctx) => {
+            const userId = args[0].value;
+            const nickname = args[1].value;
+            storage[userId] = nickname;
+            return { content: "Wpee2L System: Nickname has been set." };
+        }
+    });
+
+    this.unpatch = patcher.after("default", MessageHeader, ([{ message }], res) => {
+        const customAlias = storage[message.author.id];
+        if (customAlias) {
+            const headerChildren = res.props.children[1].props.children;
+            headerChildren.push(
+                React.createElement(Text, {
+                    style: {
+                        color: "#FF0000",
+                        fontWeight: "bold",
+                        marginLeft: 4,
+                        fontSize: 12,
+                    }
+                }, "[" + customAlias + "]")
+            );
+        }
+    });
+}
+
+function onUnload() {
+    this.unregisterCommand?.();
+    this.unpatch?.();
+}
+
 export default {
-    onLoad: () => {
-        // Plugin by Wpee2L
-        const unregister = commands.registerCommand({
-            name: "setnickname",
-            description: "Set a local red nickname (Plugin by Wpee2L)",
-            options: [
-                {
-                    name: "user",
-                    description: "The user to nickname",
-                    type: 6,
-                    required: true,
-                },
-                {
-                    name: "nick",
-                    description: "The nickname to display",
-                    type: 3,
-                    required: true,
-                }
-            ],
-            execute: (args, ctx) => {
-                const userId = args[0].value;
-                const nickname = args[1].value;
-                storage[userId] = nickname;
-                return { content: `[Wpee2L System]: Nickname "${nickname}" has been set for this user.` };
-            }
-        });
-
-        this.unregisterCommand = unregister;
-
-        this.unpatch = patcher.after("default", MessageHeader, ([{ message }], res) => {
-            const customAlias = storage[message.author.id];
-            if (customAlias) {
-                const headerChildren = res.props.children[1].props.children;
-                headerChildren.push(
-                    React.createElement(Text, {
-                        style: {
-                            color: "#FF0000",
-                            fontWeight: "bold",
-                            marginLeft: 4,
-                            fontSize: 12,
-                        }
-                    }, `[${customAlias}]`)
-                );
-            }
-        });
-    },
-    onUnload: () => {
-        if (this.unregisterCommand) this.unregisterCommand();
-        if (this.unpatch) this.unpatch();
-    }
+    onLoad,
+    onUnload
 };
