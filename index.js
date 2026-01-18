9933f8f2efc059d45b2d3d8132054e43f26f659d
anotheryousef0-tech <anotheryousef0@gmail.com>
2026-01-18 15:03:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,28 @@
-const { metro, patcher, storage, commands } = vendetta;
-const { React } = metro.common;
+const { commands, storage } = vendetta;
 
-const MessageHeader = metro.find(m => m.default?.displayName === "MessageHeader");
-const { Text } = metro.common.Components;
-
-const RedNick = {
+export default {
     onLoad: () => {
-        // Plugin by Wpee2L
-        RedNick.unregister = commands.registerCommand({
-            name: "setnickname",
-            description: "Set a local red nickname (Wpee2L System)",
-            options: [
-                {
-                    name: "user",
-                    description: "The user to nickname",
-                    type: 6,
-                    required: true,
-                },
-                {
-                    name: "nick",
-                    description: "The nickname to display",
-                    type: 3,
-                    required: true,
+        try {
+            const unregister = commands.registerCommand({
+                name: "setnickname",
+                description: "Save a local nick - Wpee2L",
+                options: [
+                    { name: "user", description: "user", type: 6, required: true },
+                    { name: "nick", description: "nick", type: 3, required: true }
+                ],
+                execute: (args) => {
+                    const userId = args[0].value;
+                    const nickname = args[1].value;
+                    storage[userId] = nickname;
+                    return { content: "Saved: " + nickname };
                 }
-            ],
-            execute: (args, ctx) => {
-                const userId = args[0].value;
-                const nickname = args[1].value;
-                storage[userId] = nickname;
-                return { content: "Wpee2L System: Local nickname saved." };
-            }
-        });
-
-        RedNick.unpatch = patcher.after("default", MessageHeader, ([{ message }], res) => {
-            const customAlias = storage[message.author.id];
-            if (customAlias) {
-                const headerChildren = res.props.children[1].props.children;
-                headerChildren.push(
-                    React.createElement(Text, {
-                        style: {
-                            color: "#FF0000",
-                            fontWeight: "bold",
-                            marginLeft: 4,
-                            fontSize: 12,
-                        }
-                    }, "[" + customAlias + "]")
-                );
-            }
-        });
+            });
+            this.unregister = unregister;
+        } catch (e) {
+            console.error(e);
+        }
     },
     onUnload: () => {
-        RedNick.unregister?.();
-        RedNick.unpatch?.();
+        if (this.unregister) this.unregister();
     }
 };
-
-export default RedNick;
