279af7528d1603a8dc241ce0927eb4f6682403aa
anotheryousef0-tech <anotheryousef0@gmail.com>
2026-01-18 14:52:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,47 @@
-import { metro, patcher, storage, commands } from "@vendetta";
-import { React } from "@vendetta/metro/common";
+const { metro, patcher, storage, commands } = vendetta;
+const { React } = metro.common;
 
 const MessageHeader = metro.find(m => m.default?.displayName === "MessageHeader");
 const { Text } = metro.common.Components;
 
 export default {
     onLoad: () => {
-        // 1. Register the Slash Command
-        this.unregisterCommand = commands.registerCommand({
+        // Plugin by Wpee2L
+        const unregister = commands.registerCommand({
             name: "setnickname",
-            description: "Set a local red nickname for a user",
+            description: "Set a local red nickname (Plugin by Wpee2L)",
             options: [
                 {
                     name: "user",
                     description: "The user to nickname",
-                    type: 6, // User type
+                    type: 6,
                     required: true,
                 },
                 {
                     name: "nick",
                     description: "The nickname to display",
-                    type: 3, // String type
+                    type: 3,
                     required: true,
                 }
             ],
             execute: (args, ctx) => {
                 const userId = args[0].value;
                 const nickname = args[1].value;
-                
-                // Save to local storage
                 storage[userId] = nickname;
-                
-                return { content: `Successfully set nickname "${nickname}" for user ${userId}.` };
+                return { content: `[Wpee2L System]: Nickname "${nickname}" has been set for this user.` };
             }
         });
 
-        // 2. Patch the Chat Header to show the nickname
-        this.unpatch = patcher.after("default", MessageHeader, ([{ message }], res) => {
-            const authorId = message.author.id;
-            const customAlias = storage[authorId];
+        this.unregisterCommand = unregister;
 
+        this.unpatch = patcher.after("default", MessageHeader, ([{ message }], res) => {
+            const customAlias = storage[message.author.id];
             if (customAlias) {
                 const headerChildren = res.props.children[1].props.children;
-                
                 headerChildren.push(
                     React.createElement(Text, {
                         style: {
-                            color: "#FF0000", // Red color
+                            color: "#FF0000",
                             fontWeight: "bold",
                             marginLeft: 4,
                             fontSize: 12,
@@ -56,9 +51,8 @@ export default {
             }
         });
     },
-
     onUnload: () => {
-        this.unregisterCommand?.();
-        this.unpatch?.();
+        if (this.unregisterCommand) this.unregisterCommand();
+        if (this.unpatch) this.unpatch();
     }
 };
